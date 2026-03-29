89dadd206d7b57450b72f1a0448b7bb867dd5dee
dmshr <dimass.heru@gmail.com>
2026-03-29 17:27:58 +0700

    test 3

--- src/app/api/boss/route.js
+++ src/app/api/boss/route.js
@@ -2,6 +2,7 @@ import { saveBoss, getAllBosses } from "@/repositories/bossRepository";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 export async function GET() {
   const bosses = await getAllBosses();
