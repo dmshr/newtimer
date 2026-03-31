dd07ebdb8da14a9d57afb223fe2152e6b293775f
dmshr <dimass.heru@gmail.com>
2026-03-31 15:55:26 +0700

    new 1

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.externals.push("@prisma/client");
-    return config;
-  },
-};
-
-export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -16,5 +16,3 @@ const config = {
   },
   plugins: [],
 };
-
-export default config;
